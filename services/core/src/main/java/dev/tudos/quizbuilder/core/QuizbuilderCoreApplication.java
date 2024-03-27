bb28831e5dd1b956654692651e6240e29449adac
Evgeniy Tudos <etdevelops@gmail.com>
2024-03-28 01:02:16 +0600

    QB-1 Configure project structure

---
+++ services/core/src/main/java/dev/tudos/quizbuilder/core/QuizbuilderCoreApplication.java
@@ -0,0 +1,13 @@
+package dev.tudos.quizbuilder.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizbuilderCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizbuilderCoreApplication.class, args);
+	}
+
+}
---
+++ shared/common-api/src/main/java/dev/tudos/quizbuilder/common/api/ResponseDto.java
@@ -0,0 +1,4 @@
+package dev.tudos.quizbuilder.common.api;
+
+public class ResponseDto {
+}
